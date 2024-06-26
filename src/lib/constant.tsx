ed3a87a93ed231834889f4b0e8c3409e229563ec
Rishav Kumar <rishavkr1766@gmail.com>
2024-06-26 22:51:06 +0530

    V4 intial commit
    Co-authored-by: Aryans2411 <Aryans2411@users.noreply.github.com>

---
+++ src/lib/constant.tsx
@@ -0,0 +1,80 @@
+export const products = [
+    {
+      title: 'Moonbeam',
+      link: 'https://gomoonbeam.com',
+      thumbnail: '/art1.jpg',
+    },
+    {
+      title: 'Cursor',
+      link: 'https://cursor.so',
+      thumbnail: '/art2.jpg',
+    },
+    {
+      title: 'Rogue',
+      link: 'https://userogue.com',
+      thumbnail: '/art3.jpg',
+    },
+  
+    {
+      title: 'Editorially',
+      link: 'https://editorially.org',
+      thumbnail: '/art4.jpg',
+    },
+    {
+      title: 'Editrix AI',
+      link: 'https://editrix.ai',
+      thumbnail: '/art5.jpg',
+    },
+    {
+      title: 'Pixel Perfect',
+      link: 'https://app.pixelperfect.quest',
+      thumbnail: '/art6.jpg',
+    },
+  
+    {
+      title: 'Algochurn',
+      link: 'https://algochurn.com',
+      thumbnail: '/art1.jpg',
+    },
+    {
+      title: 'Aceternity UI',
+      link: 'https://ui.aceternity.com',
+      thumbnail: '/art2.jpg',
+    },
+    {
+      title: 'Tailwind Master Kit',
+      link: 'https://tailwindmasterkit.com',
+      thumbnail: '/art3.jpg',
+    },
+    {
+      title: 'SmartBridge',
+      link: 'https://smartbridgetech.com',
+      thumbnail: '/art4.jpg',
+    },
+    {
+      title: 'Renderwork Studio',
+      link: 'https://renderwork.studio',
+      thumbnail: '/art5.jpg',
+    },
+  
+    {
+      title: 'Creme Digital',
+      link: 'https://cremedigital.com',
+      thumbnail: '/art6.jpg',
+    },
+    {
+      title: 'Golden Bells Academy',
+      link: 'https://goldenbellsacademy.com',
+      thumbnail: '/art1.jpg',
+    },
+    {
+      title: 'Invoker Labs',
+      link: 'https://invoker.lol',
+      thumbnail: '/art2.jpg',
+    },
+    {
+      title: 'E Free Invoice',
+      link: 'https://efreeinvoice.com',
+      thumbnail: '/art3.jpg',
+    },
+  ]
