d30c62ead1c24dd2ff0ab8c7d33306553ca1251c
Rishav Kumar <rishavkr1766@gmail.com>
2024-06-27 23:06:04 +0530

    feat: Add LayoutGridDemo component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import {  HeroParallax } from "../components/ui/hero-parallax";
 import  Header  from "../components/global/header";
 import { products } from "@/lib/constant";
 import Featured from "@/components/global/featured";
+import { LayoutGridDemo } from "@/components/global/layout-grid-demo";
 export default function Home() {
   return (
     <main >
       <Header />
       <HeroParallax products={products}/>
       <Featured />
+      <LayoutGridDemo />
     </main>
   );
 }
