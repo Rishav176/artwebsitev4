ca78a1afc329fba0b3307a7d81ac932a60c5793c
Rishav Kumar <rishavkr1766@gmail.com>
2024-06-27 23:29:53 +0530

    feat: Add Products_Featured component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import  Header  from "../components/global/header";
 import { products } from "@/lib/constant";
 import Featured from "@/components/global/featured";
 import { LayoutGridDemo } from "@/components/global/layout-grid-demo";
+import Products_Featured from "@/components/global/products-featured";
 export default function Home() {
   return (
     <main >
       <Header />
       <HeroParallax products={products}/>
       <Featured />
+      <Products_Featured />
       <LayoutGridDemo />
     </main>
   );
