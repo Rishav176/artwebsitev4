2b95650f2b864f57ce19e51fee28f0c64a300942
Aryans2411 <aryansinha241103@gmail.com>
2024-07-03 00:40:34 +0530

    Co-authored-by: Rishav176 <Rishav176@users.noreply.github.com> Added Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-
 import {  HeroParallax } from "../components/ui/hero-parallax";
-
 import { products } from "@/lib/constant";
 import Featured from "@/components/global/featured";
-
 import Products_Featured from "@/components/global/products-featured";
 import PromoSection from "@/components/global/promo-section";
 import FAQ from "@/components/global/faq";
+import Footer from "@/components/global/footer"
 export default function Home() {
   return (
     <main >
@@ -16,6 +14,7 @@ export default function Home() {
       <Products_Featured />
       <PromoSection />
       <FAQ />
+      <Footer />
     </main>
   );
 }
