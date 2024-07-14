e1f45f243bad442bd8453df249ebedc87e3af922
Rishav Kumar <rishavkr1766@gmail.com>
2024-07-14 11:53:49 +0530

    shopify fix

--- src/components/global/products-featured.jsx
+++ src/components/global/products-featured.jsx
@@ -129,7 +129,8 @@ const Staticproducts = [
   
   export default async function Products_Featured(products) {
     //const prods= getAllProducts()
-    console.log(products)
+    const extractedProducts=JSON.parse(products.products.value).edges
+    console.log(extractedProducts)
     return (
       <div className="bg-white">
         <div className="py-16 sm:py-24 lg:mx-auto lg:max-w-screen-2xl lg:px-8">
@@ -147,25 +148,28 @@ const Staticproducts = [
                 role="list"
                 className="mx-4 inline-flex space-x-8 sm:mx-6 lg:mx-0 lg:grid lg:grid-cols-4 lg:gap-x-8 lg:space-x-0 "
               >
-                {Staticproducts.map((product) => (
-                  <li key={product.id} className="inline-flex w-64 flex-col text-center lg:w-auto pb-6">
+                {extractedProducts.map((item) => {
+                  const product= item.node
+                  const image= product.images.edges[0].node
+                  return (
+                  <li key={product.handle} className="inline-flex w-64 flex-col text-center lg:w-auto pb-6">
                     <div className="group relative">
                       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200">
                         <img
-                          src={product.imageSrc}
-                          alt={product.imageAlt}
+                          src={image.url}
+                          alt=''
                           className="h-full w-full object-cover object-center  group-hover:scale-125 transition-all duration-300 ease-in-out"
                         />
                       </div>
                       <div className="mt-6">
-                        <p className="text-sm text-gray-500">{product.color}</p>
+                        <p className="text-sm text-gray-500"></p>
                         <h3 className="mt-1 font-semibold text-gray-900">
-                          <a href={product.href}>
+                          <a href='/'>
                             <span className="absolute inset-0" />
-                            {product.name}
+                            {product.title}
                           </a>
                         </h3>
-                        <p className="mt-1 text-gray-900">{product.price}</p>
+                        <p className="mt-1 text-gray-900">${product.priceRange.minVariantPrice.amount}</p>
                       </div>
                     </div>
   
@@ -174,7 +178,7 @@ const Staticproducts = [
                       <Button>Purchase</Button>
                     </ul>
                   </li>
-                ))}
+                )})}
               </ul>
             </div>
           </div>
