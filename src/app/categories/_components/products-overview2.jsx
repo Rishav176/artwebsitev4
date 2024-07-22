0930ea736ab431e68e63bcb9aee8141bb3872abe
Rishav Kumar <rishavkr1766@gmail.com>
2024-07-21 22:53:13 -0600

    update product overview on small screens

--- src/app/categories/_components/products-overview2.jsx
+++ src/app/categories/_components/products-overview2.jsx
@@ -85,44 +85,44 @@ export default function ProductsOverview({name}) {
   if (!products ) return <p>No products available</p>;
 
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-          {/* Image gallery */}
-          <TabGroup className="flex flex-col-reverse">
-            {/* Image selector */}
-            <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
-              <TabList className="grid grid-cols-4 gap-6">
-                {products.images.edges.map((image) => (
-                  <Tab
-                    key={image.node.url}
-                    className="group relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
-                  >
-                    <span className="sr-only">{image.name}</span>
-                    <span className="absolute inset-0 overflow-hidden rounded-md">
-                      <img alt={products.title} src={image.node.url} className="h-full w-full object-cover object-center" />
-                    </span>
-                    <span
-                      aria-hidden="true"
-                      className="pointer-events-none absolute inset-0 rounded-md ring-2 ring-transparent ring-offset-2 group-data-[selected]:ring-indigo-500"
-                    />
-                  </Tab>
-                ))}
-              </TabList>
-            </div>
-
-            <TabPanels className="aspect-h-1 aspect-w-1 w-full">
+    <div className="bg-white mt-10 pt-10">
+    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+        {/* Image gallery */}
+        <TabGroup className="flex flex-col-reverse">
+          {/* Image selector */}
+          <div className="mx-auto mt-6 w-full max-w-2xl lg:max-w-none">
+            <TabList className="grid grid-cols-4 gap-6 sm:grid-cols-4">
               {products.images.edges.map((image) => (
-                <TabPanel key={image.id}>
-                  <img
-                    alt={products.title}
-                    src={image.node.url}
-                    className="h-full w-full object-cover object-center sm:rounded-lg"
+                <Tab
+                  key={image.node.url}
+                  className="group relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
+                >
+                  <span className="sr-only">{image.name}</span>
+                  <span className="absolute inset-0 overflow-hidden rounded-md">
+                    <img alt={products.title} src={image.node.url} className="h-full w-full object-cover object-center" />
+                  </span>
+                  <span
+                    aria-hidden="true"
+                    className="pointer-events-none absolute inset-0 rounded-md ring-2 ring-transparent ring-offset-2 group-data-[selected]:ring-indigo-500"
                   />
-                </TabPanel>
+                </Tab>
               ))}
-            </TabPanels>
-          </TabGroup>
+            </TabList>
+          </div>
+
+          <TabPanels className="aspect-h-1 aspect-w-1 w-full">
+            {products.images.edges.map((image) => (
+              <TabPanel key={image.id}>
+                <img
+                  alt={products.title}
+                  src={image.node.url}
+                  className="h-full w-full object-cover object-center sm:rounded-lg"
+                />
+              </TabPanel>
+            ))}
+          </TabPanels>
+        </TabGroup>
 
           {/* Product info */}
           <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
