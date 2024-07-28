e96f324c53f9edc4a7312684ad998e259c638884
Rishav Kumar <rishavkr1766@gmail.com>
2024-07-27 23:53:50 -0600

    update header, remove unnecessary elements

--- src/components/global/header.jsx
+++ src/components/global/header.jsx
@@ -21,12 +21,12 @@ export default function Header() {
       <nav aria-label="Global" className="mx-auto flex items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <Link href="/" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img
+            <span className="sr-only">RituHandmadeArts</span>
+            {/* <img
               alt=""
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               className="h-8 w-auto"
-            />
+            /> */}
           </Link>
         </div>
         <div className="flex lg:hidden">
@@ -47,9 +47,9 @@ export default function Header() {
           ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          {/* <Link href="#" className="text-sm font-semibold leading-6 text-gray-900">
             Log in <span aria-hidden="true">&rarr;</span>
-          </Link>
+          </Link> */}
         </div>
       </nav>
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
